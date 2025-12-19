95eba7a290c01da79a9b79e8c65d09e71002cb8f
JulieLUBET <julie.lubet@epita.fr>
2025-12-19 11:57:04 +0100

    essaie correction github

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import About from "./components/About"
 import Experiences from "./components/Experiences"
 import Footer from "./components/Footer"
-import Home from "./components/home"
+import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 
