faafcd4a5c3227a13f089e24360e5ca3ffb4168d
JulieLUBET <julie.lubet@epita.fr>
2026-02-24 12:33:42 +0100

    correction erreur

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import ContactSection from "../components/ContactSection";
-/*import ExperiencesSection from "../components/ExperiencesSection";*/
+/*import ContactSection from "../components/ContactSection";
+import ExperiencesSection from "../components/ExperiencesSection";*/
 import Hero from "../components/Hero";
 import ProjectsSection from "../components/ProjectsSection";
 import SkillsSection from "../components/SkillsSection";
